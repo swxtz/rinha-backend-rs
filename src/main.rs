9ec6127374fae10df14a3aee5a06bec5a9dc8523
Gustavo Mendonça <alf4r6@gmail.com>
2023-09-03 14:37:31 -0300

    feat: serde json

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use axum::{
+    extract::{Path, State},
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
-    Router,
+    Router, Json,
 };
 use time::{macros::date, Date};
 use uuid::Uuid;
+use serde::Serialize;
 
+#[derive(Clone, Serialize)]
 pub struct Person {
     pub id: Uuid,
     pub name: String,
@@ -16,6 +19,8 @@ pub struct Person {
     pub birth_date: Date,
     pub stack: Vec<String>,
 }
+
+type AppState = Arc<HashMap<Uuid, Person>>;
 
 #[tokio::main]
 async fn main() {
@@ -30,13 +35,17 @@ async fn main() {
         stack: vec!["Rust".to_string(), "Typescript".to_string()],
     };
 
+    println!("{}", person.id);
     people.insert(person.id, person);
+
+    let app_state = Arc::new(people);
 
     let app = Router::new()
         .route("/pessoas", get(get_person))
         .route("/pessoas", post(create_person))
         .route("/pessoas/:id", get(get_person_by_id))
-        .route("/contagem-pessoas", get(count_person));
+        .route("/contagem-pessoas", get(count_person))
+        .with_state(app_state);
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -48,8 +57,17 @@ async fn get_person() -> impl IntoResponse {
     "pessoas"
 }
 
-async fn get_person_by_id() -> impl IntoResponse {
-    "pessoa por id"
+async fn get_person_by_id(
+    State(people): State<AppState>,
+    Path(person_id): Path<Uuid>,
+) -> impl IntoResponse {
+
+    let person = people.get(&person_id);
+
+    match person {
+        Some(person) => Ok(Json(person.clone())),
+        None => Err((StatusCode::NOT_FOUND, "pessoa não encontrada")),
+    }
 }
 
 async fn create_person() -> impl IntoResponse {
